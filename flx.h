43bbd2546771b81a3a71667145da8d6883d54e27
FLX05PLS <1242755081@qq.com>
2025-09-20 22:27:08 +0800

    feat: initial commit

---
+++ flx.h
@@ -0,0 +1,1 @@
+#define FLX 1000
---
+++ text.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#include"flx.h"
+
+int main()
+{
+
+    printf("hello world\n");
+    printf("the num \t of flx: %d", FLX);
+    getchar();  // 等待用户按下回车才退出
+    printf("the num \t of flx: %d", FLX);
+    getchar();  // 等待用户按下回车才退出
+    printf("the num \t of flx: %d", FLX);
+    getchar();  // 等待用户按下回车才退出
+    // return 0;
+}
